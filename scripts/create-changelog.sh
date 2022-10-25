58c4135533d21015e6f3eba9fb95d59e5e4f15bd
Urip Subekti <imanuel.subekti@efishery.com>
2022-10-26 02:20:43 +0700

    chore: fix again

--- scripts/create-changelog.sh
+++ scripts/create-changelog.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 NAME_PKG="test-pkg"
 LAST_TAG=$(git describe --tags --abbrev=0)
-MESSAGES=$(git log $LAST_TAG..HEAD --no-merges --oneline --pretty=tformat:"%s")
+
+if [ -z "$LAST_TAG" ]; then
+   MESSAGES=$(git log --no-merges --oneline --pretty=tformat:"%s")
+   echo "MESSAGES: $MESSAGES"
+else
+   MESSAGES=$(git log $LAST_TAG..HEAD --no-merges --oneline --pretty=tformat:"%s")
+   echo "MESSAGES: $MESSAGES"
+fi
 
 IFS=$'\n' read -rd '' -a array <<<"$MESSAGES"
 
 for index in "${!array[@]}"; do
-   echo "---\n\"${NAME_PKG}\": patch\n---\n\n${array[index]}" > "./.changeset/update-${index}.md"
+   cat >>./.changeset/update-${index}.md <<EOF
+   ---
+   "${NAME_PKG}": patch
+   ---
+
+   ${array[index]}
+EOF
 done
